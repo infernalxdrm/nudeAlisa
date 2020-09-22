e393c6298093d126d4445e1729a31daa687e7c3e
Kworker <mixteamlp@gmail.com>
2020-09-22 19:31:20 +1000

    added file sending test method
    Signed-off-by: Kworker <mixteamlp@gmail.com>

--- src/main/java/core/Commands.java
+++ src/main/java/core/Commands.java
@@ -1,14 +1,30 @@
 package core;
 
+import discord4j.core.spec.MessageCreateSpec;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.function.Consumer;
 
 public class Commands {
     static final HashMap<String,Command> commands=new HashMap<>();
     public static void   setup(){
-       commands.put("hello", event->event.getMessage().
-               getChannel().flatMap(chanel->
+        //Add all commands down here
+       commands.put("hello", event->event.getMessage()
+               .getChannel().flatMap(chanel->
                chanel.createMessage("Hello"+event.getMessage().getAuthor().get().getMention())
                        .then()));
-        //Add all commands here
+       commands.put("test_file",event -> event.getMessage()
+       .getChannel()
+               .flatMap(chanel->chanel.createMessage(messageCreateSpec -> {
+                   try {
+                       messageCreateSpec.addFile("test",new FileInputStream("C:/Users/User/Pictures/test.png"));
+                   } catch (FileNotFoundException e) {
+                       e.printStackTrace();
+                   }
+               })
+
+               .then()));
     }
 }
