ef4c19cbe729241c80f601d13b98daf3cad01d33
Kworker <mixteamlp@gmail.com>
2020-09-23 20:55:55 +1000

    added music queue
    Signed-off-by: Kworker <mixteamlp@gmail.com>

--- src/main/java/core/services/TrackScheduler.java
+++ src/main/java/core/services/TrackScheduler.java
@@ -6,22 +6,45 @@ import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public final class TrackScheduler implements AudioLoadResultHandler {
 
     private final AudioPlayer player;
+    Queue<AudioTrack> playlist = new LinkedList<>();
 
     public TrackScheduler(final AudioPlayer player) {
         this.player = player;
+        //checks the playlist and plays music when one is finished
+        new Thread(()->{
+            while (true) {
+                try {
+                    if (playlist.isEmpty()){
+                        Thread.sleep(200);
+                        continue;
+                    }
+                        Thread.sleep(200);
+                    if (player.getPlayingTrack() == null) player.playTrack(playlist.poll());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     @Override
     public void trackLoaded(final AudioTrack track) {
         // LavaPlayer found an audio source for us to play
-        player.playTrack(track);
+        playlist.add(track);
+
+       // player.playTrack(track);
+
     }
 
     @Override
     public void playlistLoaded(final AudioPlaylist playlist) {
+        this.playlist.addAll(playlist.getTracks());
         // LavaPlayer found multiple AudioTracks from some playlist
     }
 
