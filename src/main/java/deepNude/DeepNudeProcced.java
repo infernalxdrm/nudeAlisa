80499ab4dbc0244ae1c3826edef27aa68da842a9
Kworker <mixteamlp@gmail.com>
2021-01-28 22:05:33 +1000

    added deepnude
    Signed-off-by: Kworker <mixteamlp@gmail.com>

--- src/main/java/deepNude/DeepNudeProcced.java
+++ src/main/java/deepNude/DeepNudeProcced.java
@@ -7,12 +7,26 @@ import reactor.core.publisher.Mono;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class DeepNudeProcced {
 
     public Mono<Void> getDeepNude(MessageCreateEvent event) {
         event.getMessage().getAttachments().forEach(attachment -> {
-            ImagePr.writeImage(ImagePr.readImage(attachment.getUrl()), "/DeepNude_NoWatermark_withModel/input.png", "png");
+            try {
+                Files.delete(Paths.get(System.getProperty("user.dir") + "/DeepNude_NoWatermark_withModel/input.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            ImagePr.writeImage(ImagePr.readImage(attachment.getProxyUrl()), System.getProperty("user.dir") + "/DeepNude_NoWatermark_withModel/input.png", "png");
+            while (Files.notExists(Paths.get(System.getProperty("user.dir") + "/DeepNude_NoWatermark_withModel/input.png"))) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
             synchronized (DeepNudeProcced.class) {
                 try {
                     String invoke = "python3 /DeepNude_NoWatermark_withModel/main.py;";
@@ -28,7 +42,7 @@ public class DeepNudeProcced {
                         event.getMessage().getChannel().block().createMessage(
                                 r -> {
                                     try {
-                                        r.addFile("nude.png", new FileInputStream("/DeepNude_NoWatermark_withModel/output.png"));
+                                        r.addFile("nude.png", new FileInputStream(System.getProperty("user.dir") + "/DeepNude_NoWatermark_withModel/output.png"));
                                     } catch (FileNotFoundException e) {
                                         e.printStackTrace();
                                     }
