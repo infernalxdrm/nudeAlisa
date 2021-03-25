96f51f85d9178657b373a15dfc933cb24999c764
Kworker <mixteamlp@gmail.com>
2021-03-25 22:01:56 +1000

    added previews for instagram
    Signed-off-by: Kworker <mixteamlp@gmail.com>

--- src/main/java/instagram/instagram.java
+++ src/main/java/instagram/instagram.java
@@ -26,7 +26,7 @@ public class instagram {
         httpCon.addRequestProperty("Cache-Control", "max-age=0");
         httpCon.addRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
         httpCon.addRequestProperty("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36");
-        httpCon.addRequestProperty("Accept-Encoding", "UTF-8");
+        // httpCon.addRequestProperty("Accept-Encoding", "UTF-8");
         // httpCon.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
         //httpCon.addRequestProperty("Cookie", "JSESSIONID=EC0F373FCC023CD3B8B9C1E2E2F7606C; lang=tr; __utma=169322547.1217782332.1386173665.1386173665.1386173665.1; __utmb=169322547.1.10.1386173665; __utmc=169322547; __utmz=169322547.1386173665.1.1.utmcsr=stackoverflow.com|utmccn=(referral)|utmcmd=referral|utmcct=/questions/8616781/how-to-get-a-web-pages-source-code-from-java; __gads=ID=3ab4e50d8713e391:T=1386173664:S=ALNI_Mb8N_wW0xS_wRa68vhR0gTRl8MwFA; scrElm=body");
         HttpURLConnection.setFollowRedirects(false);
@@ -54,7 +54,7 @@ public class instagram {
 
         //System.out.println(links.toString());
         links.removeIf(s -> s.contains("script"));
-        links.forEach(System.out::println);
+        //    links.forEach(System.out::println);
         httpCon.disconnect();
         return links;
     }
